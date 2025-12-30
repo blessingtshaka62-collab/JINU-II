7ecf32e79c6504ab5f481f0323fc572065c57a7a
blessingtshaka62-collab <blessingtshaka62@gmail.com>
2025-12-30 12:56:02 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author:'ɢʟᴀᴅᴅʏ ᴋɪɴɢ',
   botName: "ᴊɪɴᴜ-ɪɪ",
   botOwner: 'ɢʟᴀᴅᴅʏ ᴋɪɴɢ', // Your name
-  ownerNumber: '263775953409', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '27678814338', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
